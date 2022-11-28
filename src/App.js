a058f311dfe704052bff4a0d8e095543f10c306a
milkbreadzee <riaelisabeths@gmail.com>
2022-11-28 20:45:54 +0530

    expand on scroll cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import {motion, useInView, useScroll, Variants} from 'framer-motion';
+import { useRef, useState } from 'react';
+import "./App.css"
+
 
 function App() {
+
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+  const[isOpen, setisOpen] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+          <section >
+          <motion.div relative
+            transition = {{layout:{duration:1,type:'spring'}}}
+            layout 
+            onClick={() => setisOpen(!isOpen)} 
+            className='card'
+            style={{borderRadius: '3rem'}}>
+                <motion.h2 layout='position'>
+                  ria
+                </motion.h2>
+                {isOpen &&
+                <motion.div initial={{opacity:0}}
+                whileInView={{opacity:1}}
+                animate={{opacity:1}}
+                transition={{duration:1}}
+                className='expand'>
+                <p> hi! im ria, im learning framer motion in react to become a better front end developer !! </p>
+                </motion.div>
+            } 
+          </motion.div>
+          </section>
+
+          <section ref = {ref}>
+          <motion.div relative
+            transition = {{layout:{duration:1,type:'spring'}}}
+            layout 
+            onClick={() => setisOpen(!isOpen)} 
+            className='card'
+            style={{borderRadius: '3rem'}}>
+                <motion.h2 layout='position'>
+                  ria 2
+                </motion.h2>
+                {isInView &&
+                <motion.div
+                initial={{opacity:0}}
+                animate={{opacity:1}}
+                transition={{duration:1}}
+                className='expand'>
+                <p> hi! im ria, im learning framer motion in react to become a better front end developer !! </p>
+                </motion.div>
+            } 
+          </motion.div>
+          </section>
+        </div>
+     
   );
 }
 
